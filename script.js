88f0435b1d3cb07316dbad3837f8443ecc8f5580
Abylai <aba.omar@mail.ru>
2022-09-26 11:14:53 +0600

    HW03

---
+++ script.js
@@ -0,0 +1,5 @@
+function showName() {
+    user_name = prompt("Введите имя");
+    document.getElementById("result").innerHTML = ""
+    document.getElementById("result").append(`Ваше имя: ${user_name}`);
+}
